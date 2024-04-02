7a2103fb6e8126ae266bd45f40df4c4fa5d0bb8e
Alberto Bruno Silvestre de Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2024-04-02 15:17:37 -0300

    Ultimos detalhes

--- main.py
+++ main.py
@@ -13,15 +13,36 @@ for i in range(0, num_cidades):
     cidades.append(str(input(f"Digite o nome da {i+1}ª cidade: ").upper()))
     ufs.append(str(input("Digite a Unidade Federativa: ").upper()))
 
-
+system("CLS")
+print("Obtendo Latitudes e Longitudes...")
 latitudes_e_longitudes = dicionario_cidades(cidades, ufs)
-
+print()
 for i in range(0, num_cidades):
-    chave1 = f"{cidades[i] - ufs[i]}"
+    chave1 = f"{cidades[i]} - {ufs[i]}"
 
-    for j in range(0, num_cidades):
-        chave2 = f"{cidades[j] - ufs[j]}"
+    for j in range(i+1, num_cidades):
+        chave2 = f"{cidades[j]} - {ufs[j]}"
 
         distancia = distancia_entre_cidades(latitudes_e_longitudes, chave1, chave2)
 
         distancias.append([chave1, chave2, distancia])
+
+        print(f"{chave1}\t{chave2}\t{distancia}")
+
+resp = str(input("\nDeseja salvar estes dados em um CSV? [S/N]: ")).lower()
+
+if resp == 's':
+    nome_arquivo = input("Digite o nome do arquivo: ").lower()
+
+    if nome_arquivo.find(".csv") == -1:
+        nome_arquivo += ".csv"
+    
+    with open(nome_arquivo, 'w') as arq:
+        arq.write("cidade1,cidade2,distancia\n")
+
+        for i in range(0, len(distancias)):
+            arq.write(f"{distancias[i][0]},{distancias[i][1]},{distancias[i][2]}\n")
+    
+    print("Arquivo salvo com sucesso!")
+
+print("\nPrograma finalizado com sucesso :)")
